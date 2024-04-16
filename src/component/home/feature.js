d0d96858bf89ca07adc29f2da21d8e5da428adb6
HungITic <87306925+HungITic@users.noreply.github.com>
2024-04-16 23:29:05 +0700

    .

--- src/component/home/feature.js
+++ src/component/home/feature.js
@@ -21,6 +21,7 @@ export default function Feature() {
     {
       name: "TOEIC",
       subText: "Sự chuẩn bị toàn diện cho kỳ thi TOEIC",
+      link: "",
     },
   ];
 
@@ -29,29 +30,31 @@ export default function Feature() {
       <Row
         gutter={[
           { xs: 14, sm: 14, md: 20, xl: 30 },
-          { xs: 14, sm: 14, md: 20, xl: 30 },
+          { xs: 30, sm: 30, md: 20, xl: 30 },
         ]}
         className={"listFeature"}
       >
         {MARKER_FEATURE.map((item) => (
           <Col xs={24} sm={24} md={8} xl={8}>
-            <div className="itemFeature">
-              <div className="itemImage">
-                {item.name === "TOEIC" ? (
-                  <div className="coming">SẮP RA MẮT</div>
-                ) : (
-                  <Image
-                    src={item.image}
-                    className="serviceImage"
-                    alt="image"
-                  ></Image>
-                )}
+            <Link href={item?.link}>
+              <div className="itemFeature">
+                <div className="itemImage">
+                  {item.name === "TOEIC" ? (
+                    <div className="coming">SẮP RA MẮT</div>
+                  ) : (
+                    <Image
+                      src={item.image}
+                      className="serviceImage"
+                      alt="image"
+                    ></Image>
+                  )}
+                </div>
+                <div className="itemTitle">
+                  <h3>{item.name}</h3>
+                  <p>{item.subText}</p>
+                </div>
               </div>
-              <div className="itemTitle">
-                <h3>{item.name}</h3>
-                <p>{item.subText}</p>
-              </div>
-            </div>
+            </Link>
           </Col>
         ))}
       </Row>
